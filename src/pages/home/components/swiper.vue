7d58a5ea2d63ca27d48b681dce866746ea15479b
张宏扬 <zhanghongyang@zhanghongyangdeMacBook-Air.local>
2020-11-18 16:51:24 +0800

    change

--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="wrapper">
     <swiper :options="swiperOption">
-      <swiper-slide>
-        <img class="swiper-img" src="http://img1.qunarzz.com/sight/p0/1606/c0/c037adf099fd76b490.water.jpg_375x170_cd15a7a1.jpg">
-      </swiper-slide>
-      <swiper-slide>
-        <img class="swiper-img" src="//img1.qunarzz.com/sight/p0/2008/b9/b9cc3ab2fbe4f0e7a3.water.jpg_600x330_41191030.jpg">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img :src="item.imgUel" alt="" class="swiper-img">
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
@@ -17,8 +14,17 @@ export default {
   data () {
     return {
       swiperOption: {
-        pagination: '.swiper-pagination'
-      }
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUel: 'http://img1.qunarzz.com/sight/p0/1606/c0/c037adf099fd76b490.water.jpg_375x170_cd15a7a1.jpg'
+      },
+      {
+        id: '002',
+        imgUel: '//img1.qunarzz.com/sight/p0/2008/b9/b9cc3ab2fbe4f0e7a3.water.jpg_600x330_41191030.jpg'
+      }]
     }
   }
 }
@@ -27,12 +33,15 @@ export default {
 <style scoped>
 .wrapper{
   width: 100%;
-  padding-bottom: 31.25%;
+  padding-bottom: 55%;
   height: 0;
   overflow: hidden;
   background: #eee;
 }
 .swiper-img{
   width: 100%;
+}
+.wrapper >>> .swiper-pagination-bullet-active{
+  background: #fff;
 }
 </style>
